31ef58729727b9e9bd985c682a07c34f7ced4cb5
himanikapadia <kapadiahimani6@gmail.com>
2026-07-16 18:33:36 +0530

    Feat: Inventory Class added!!

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Product:
         self.__quantity=quantity
         self.__expiry_date=expiry_date
     
-    def display_product(self):
+    def display(self):
         print("==== Product Details ====")
         print(f"Id: {self.__id}")
         print(f"Name: {self.name}")
@@ -17,8 +17,30 @@ class Product:
     def update_qty(self,amount):
         self.__quantity+=amount
 
+    def get_id(self):
+        return self.__id
+
     def is_low_stock(self):
         return self.__quantity<5
+
+class Inventory:
+    def __init__(self):
+        self.__products=[]
+
+    def add_products(self,product):
+        self.__products.append(product)
+
+    def display_product(self):
+        for product in self.__products:
+            product.display()
+
+    def search_product(self,product_id):  #search by ID
+        for p in self.__products:
+            if p.get_id()==product_id:
+                print("--- Product found! ---")
+                return p
+            else:
+                return none
 
 
 
