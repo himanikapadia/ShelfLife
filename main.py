694b52c84c8d04650368957460d57920199aa076
himanikapadia <kapadiahimani6@gmail.com>
2026-07-20 22:50:47 +0530

    Removed Inventory class to be add in another folder

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from datetime import datetime
+from inventory import Inventory
+# from product import Product
+# from food import Food
+# from medicine import Medicine
+# from electronics import Electronics
 class Product:
     def __init__(self,id,name,category,quantity,expiry_date):
         self.__id=id
@@ -55,64 +60,6 @@ class Product:
 
         return (expiry - today).days
 
-class Inventory:
-    def __init__(self):
-        self.__products=[]
-
-    def add_product(self,product):
-        self.__products.append(product)
-
-    def display_products(self):
-        if not self.__products:
-            print("Inventory is Empty!")
-        else:
-            print("==== Product Details ====")
-            for product in self.__products:
-                product.display()
-
-    def search_by_Id(self,product_id):  
-        for p in self.__products:
-            if p.get_id()==product_id:
-                return p
-    def search_by_name(self,name):
-        for p in self.__products:
-            if p.get_name()==name:
-                return p
-    def search_by_category(self,category):
-        products=[]
-        for p in self.__products:
-            if p.get_category().lower()==category.lower():
-                products.append(p)
-        return products
-
-    def remove_product(self,id):
-        for p in self.__products:
-            if p.get_id()==id:
-                self.__products.remove(p)
-                print(f"Product: {id} is Removed Successfully!")
-                return True
-        print("Failed Remove!")
-        return False
-    def low_stock_products(self):
-        low_stock=[]
-        for product in self.__products:
-            if product.is_low_stock():
-                low_stock.append(product)
-        return low_stock
-    def expiry_report(self):
-        print("\n====== Expiry Report ======")
-        for product in self.__products:
-            days = product.days_left()
-            product.display()
-            if days < 0:
-                print("Expired")
-            elif days == 0:
-                print("⚠ Expires Today")
-            elif days <= 7:
-                print(f"⚠ Expires in {days} day(s)")
-            else:
-                print(f"{days} day(s) remaining")
-            print()
 
 inv=Inventory()
 
