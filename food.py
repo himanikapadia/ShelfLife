cb5396f60986898d6c8ae722a5baa394d6e426e3
himanikapadia <kapadiahimani6@gmail.com>
2026-07-21 22:11:21 +0530

    Feat(v1): Added food.py file for Inheritance

---
+++ food.py
@@ -0,0 +1,11 @@
+from product import Product
+class food(Product):
+    def __init__(self, id, name, category, quantity, expiry_date,storage_type):
+        super().__init__(id, name, category, quantity, expiry_date)
+        self.storage_type=storage_type
+
+    def display(self):
+        super().display()  
+        print("Category: FOOD")
+        print(f"Storage Type: {self.storage_type}")
+        print("-"*40)  
