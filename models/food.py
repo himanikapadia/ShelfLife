fb2c8ac86c3b09d930e499437048b9f19fb895ad
himanikapadia <kapadiahimani6@gmail.com>
2026-07-25 21:00:45 +0530

    Created Getters to be accessed by file handler class

--- models/food.py
+++ models/food.py
@@ -12,3 +12,9 @@ class Food(Product):
 
     def get_category(self):
         return "Food"
+
+    def get_expiry(self):
+        return self.__expiry_date
+
+    def get_storage(self):
+        return self.storage_type
